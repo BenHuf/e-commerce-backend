e0712105bf15bf0d2a7369b5601b4e1194566054
BenHuf <benthehuffman@gmail.com>
2022-07-03 00:28:51 -0400

    Models finished

--- models/Category.js
+++ models/Category.js
@@ -5,16 +5,36 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'category',
-  }
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'category',
+    }
 );
 
 module.exports = Category;
+
+// Category
+// id
+// Integer
+// Doesn't allow null values
+// Set as primary key
+// Uses auto increment
+// category_name
+// String
+// Doesn't allow null values
--- models/Product.js
+++ models/Product.js
@@ -8,16 +8,73 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+    {
+        // define columns
+
+        // id
+        // Integer
+        // Doesn't allow null values
+        // Set as primary key
+        // Uses auto increment
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        // product_name
+        // String
+        // Doesn't allow null values
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        // price
+        // Decimal
+        // Doesn't allow null values
+        // Validates that the value is a decimal
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+
+        // stock
+        // Integer
+        // Doesn't allow null values
+        // Set a default value of 10
+        // Validates that the value is numeric
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            default: 10,
+            validate: {
+                isNumeric: true
+            }
+        },
+
+        // category_id
+        // Integer
+        // References the category model's id
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product',
+    }
 );
 
 module.exports = Product;
